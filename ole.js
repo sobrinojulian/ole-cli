75ad42f27b7c3b10131cf7d28d81f9d7f9ad8e12
Sobrino Julian <sobrinojulian@protonmail.com>
2018-03-31 08:17:29 -0300

    done table

--- ole.js
+++ ole.js
@@ -4,6 +4,8 @@ const R = require('ramda')
 const table = require('text-table')
 const moment = require('moment')
 const axios = require('axios')
+const chalk = require('chalk')
+const Table = require('cli-table2')
 
 const fixFecha = (fecha) => {
   const torneos = R.map(fixTorneo, fecha.torneos)
@@ -32,33 +34,68 @@ const fixTorneo = (torneo) => {
 
 const fixEvento = (evento) => {
   const nombre = evento.nombre.replace('\t', '')
+  const equipos = nombre.includes(' - ') ? nombre.split(' - ') : []
   const deporte = evento.deporte.nombre
   const canales = R.map((e) => e.nombre, evento.canales)
   const fecha = moment(evento.fecha)
-  return R.merge(evento, { nombre, deporte, canales, fecha })
+  return R.merge(evento, { nombre, equipos, deporte, canales, fecha })
 }
 
 const makeTable = (agenda) => {
-  let table = []
-  moment.locale('es')
+  let table = new Table({
+    chars: {
+      top: '',
+      'top-mid': '',
+      'top-left': '',
+      'top-right': '',
+      bottom: '',
+      'bottom-mid': '',
+      'bottom-left': '',
+      'bottom-right': '',
+      left: '',
+      'left-mid': '',
+      mid: '',
+      'mid-mid': '',
+      right: '',
+      'right-mid': '',
+      middle: ' '
+    },
+    style: { 'padding-left': 0, 'padding-right': 0 },
+    colWidths: [, , 22],
+    wordWrap: true
+  })
   for (const fecha of agenda) {
     table.push([
-      moment(fecha.fecha)
-        .locale('es')
-        .format('dddd DD [de] MMMM')
-        .toUpperCase()
+      {
+        hAlign: 'center',
+        colSpan: 3,
+        content: `\n\n${chalk.bold.underline(
+          moment(fecha.fecha)
+            .locale('es')
+            .format('dddd DD [de] MMMM')
+            .toUpperCase()
+        )}`
+      }
     ])
     for (const torneo of fecha.torneos) {
-      table.push([`${torneo.emoji}  ${torneo.nombre.toUpperCase()}`])
+      table.push([
+        {
+          colSpan: 3,
+          content: `\n${torneo.emoji}  ${chalk.green(
+            torneo.nombre.toUpperCase()
+          )}`
+        }
+      ])
       for (const evento of torneo.eventos) {
         table.push([
-          `${evento.fecha.format('hh:ss')} ${evento.nombre}`,
-          evento.canales
+          chalk.red(evento.fecha.format('hh:ss')),
+          evento.equipos.length !== 0
+            ? evento.equipos.join('\n')
+            : evento.nombre,
+          { hAlign: 'right', content: evento.canales.join('\n') }
         ])
       }
-      table.push([])
     }
-    table.push([])
   }
   return table
 }
@@ -74,7 +111,8 @@ async function printAgenda () {
       R.map(fixFecha),
       makeTable
     )(data)
-    console.log(table(agenda, { align: ['l', 'r'] }))
+    console.log(agenda.toString())
+    // console.log(table(agenda, { align: ['l', 'r'] }))
   } catch (error) {
     console.error(error)
   }
